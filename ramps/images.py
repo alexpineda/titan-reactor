4caf246ce504b2fdfb698c68325b311f9f5c8156
Alex Pineda <alexpineda86@gmail.com>
2020-07-03 15:20:55 -0400

    ramp progress

--- ramps/images.py
+++ ramps/images.py
@@ -1,6 +1,11 @@
 import Image
+import ImageDraw
+# from PIL import Image, ImageDraw
 import sys
 import math
+from os import listdir
+from os.path import isfile, join
+
 
 # Open an Image
 
@@ -19,7 +24,7 @@ def save_image(image, path):
 
 
 def create_image(i, j):
-    image = Image.new("RGB", (i, j), "white")
+    image = Image.new("RGB", (i, j), "black")
     return image
 
 # Get the pixel from the given image
@@ -28,7 +33,7 @@ def create_image(i, j):
 def get_pixel(image, i, j):
     # Inside image bounds?
     width, height = image.size
-    if i > width or j > height or i < 0 or j < 0:
+    if i >= width or j >= height or i < 0 or j < 0:
         return None
 
     # Get Pixel
@@ -37,39 +42,10 @@ def get_pixel(image, i, j):
 
 # Create a Grayscale version of the image
 
-
-def convert_grayscale(image):
-    # Get size
-    width, height = image.size
-
-    # Create new Image and a Pixel Map
-    new = create_image(width, height)
-    pixels = new.load()
-
-    # Transform to grayscale
-    for i in range(width):
-        for j in range(height):
-            # Get Pixel
-            pixel = get_pixel(image, i, j)
-
-            # Get R, G, B values (This are int from 0 to 255)
-            red = pixel[0]
-            green = pixel[1]
-            blue = pixel[2]
-
-            # Transform to grayscale
-            gray = (red * 0.299) + (green * 0.587) + (blue * 0.114)
-
-            # Set Pixel in new image
-            pixels[i, j] = (red, 0, 0)
-
-        # Return new image
-        return new
-
 # ltr htl
 
 
-def find_ramps(image):
+def find_edges(image):
     # Get size
     width, height = image.size
 
@@ -83,6 +59,9 @@ def find_ramps(image):
 
     lastcolor = (-1, -1, -1)
 
+    # for y in range(height):
+    #     for x in range(width):
+    #         pixels[x, y] = black
     # Transform to grayscale
     for y in range(height):
         for x in range(width):
@@ -92,16 +71,112 @@ def find_ramps(image):
             if (pixel == black):
                 continue
 
-            prev_pixel = get_pixel(image, x-1, y)
-            pixels[x, y] = pixel
-
-            if (prev_pixel > pixel):
+            high_pixel = get_pixel(image, x-1, y)
+            if (high_pixel > pixel):
                 pixels[x, y] = (255, 0, 0)
-            else:
-                pixels[x, y] = (pixel[0]//2, pixel[1]//2, pixel[2]//2)
+
+        for x in range(width - 1, -1, -1):
+            # Get Pixel
+            pixel = get_pixel(image, x, y)
+            if (pixel == black or not pixels[x, y] == black):
+                continue
+
+            high_pixel = get_pixel(image, min(width - 1, x+1), y)
+            if (high_pixel > pixel):
+                pixels[x, y] = (0, 255, 0)
+
+    for x in range(width):
+        for y in range(height):
+            pixel = get_pixel(image, x, y)
+            if (pixel == black or not pixels[x, y] == black):
+                continue
+
+            high_pixel = get_pixel(image, x, y-1)
+            if (high_pixel > pixel):
+                pixels[x, y] = (0, 0, 255)
+
+        for y in range(height - 1, -1, -1):
+            pixel = get_pixel(image, x, y)
+            if (pixel == black or not pixels[x, y] == black):
+                continue
+
+            high_pixel = get_pixel(image, x, min(height - 1, y+1))
+            if (high_pixel > pixel):
+                pixels[x, y] = (0, 255, 255)
+
+    # for y in range(height):
+    #     for x in range(width):
+    #         if pixels[x, y] == black:
+    #             (r, g, b) = get_pixel(image, x, y)
+    #             pixels[x, y] = (
+    #                 r//4, g//4, b//4)
 
     # Return new image
     return new
+
+
+def create_ramps(image, original):
+    width, height = image.size
+    black = (0, 0, 0)
+
+    # Create new Image and a Pixel Map
+    new = create_image(width, height)
+    pixels = new.load()
+
+    processedImage = create_image(width, height)
+    processed = processedImage.load()
+
+    def find_neighbours(pos):
+        (x, y) = pos
+        if x < 0 or y < 0 or x >= width or y >= height:
+            return []
+        # no feature or feature already processed
+        if (get_pixel(image, x, y) == black or not processed[x, y] == black):
+            return []
+
+        processed[x, y] = (255, 255, 255)
+        east = find_neighbours((x+1, y))
+        south_east = find_neighbours((x+1, y+1))
+        south = find_neighbours((x, y+1))
+        south_west = find_neighbours((x-1, y+1))
+        west = find_neighbours((x-1, y))
+        north_west = find_neighbours((x-1, y-1))
+        north = find_neighbours((x, y-1))
+        north_east = find_neighbours((x+1, y-1))
+
+        return east + south_east + south + south_west + west + north_west + north + north_east + [pos]
+
+    for y in range(height):
+        for x in range(width):
+            processed[x, y] = black
+            pixels[x, y] = get_pixel(original, x, y)
+
+    ramps = []
+    for y in range(height):
+        for x in range(width):
+            if processed[x, y] == black or not get_pixel(image, x, y) == black:
+                neighbours = find_neighbours((x, y))
+                if (len(neighbours) > 15):
+                    print (neighbours)
+                    ramps.append(neighbours)
+
+    for ramp in ramps:
+        for (x, y) in ramp:
+            pixels[x, y] = get_pixel(image, x, y)
+
+    new.show()
+    return [new, ramps]
+
+
+def draw_ramps(ramps, image):
+    width, height = image.size
+
+    # creating new Image object
+    img = Image.new("RGB", (width, height))
+
+    # create line image
+    img1 = ImageDraw.Draw(img)
+    img1.line(shape, fill="none", width=0)
 
 
 def test_next_x(image, x, y, width, max_n):
@@ -202,12 +277,21 @@ def test_mp_hockey_rev(image, pixels, x, y, hc, lc, flipX, color):
     return test_and_draw_pattern(image, pixels, pattern, x, y, flipX, color)
 
 
+def mix((r, g, b), (r2, g2, b2)):
+    return ((r+r2)/2, (g+g2)/2, (b+b2)/2)
+
+
+def darken((r, g, b)):
+    return (r/4, g/4, b/4)
+
+
 def draw_box(pixels, x, y, width, height, color):
     for i in range(width):
         for j in range(height):
             (r, g, b) = pixels[x + i, y + j]
             (r2, g2, b2) = color
-            pixels[x + i, y + j] = ((r+r2)/2, (g+g2)/2, (b+b2)/2)
+            # pixels[x + i, y + j] = ((r+r2)/2, (g+g2)/2, (b+b2)/2)
+            pixels[x + i, y + j] = color
 
 
 def fill_irregularities(image):
@@ -238,14 +322,11 @@ def fill_irregularities(image):
 
             if (high_pixel > pixel):
                 out = test_mp_axe(newImage, pixels, x, y,
-                                  high_pixel, pixel, False, magenta)
-                # if not out == 0:
-                #   x = x + out
-
+                                  high_pixel, pixel, False, high_pixel)
                 out = test_fs_longduck(newImage, pixels, x, y,
-                                       high_pixel, pixel, False, green)
+                                       high_pixel, pixel, False, high_pixel)
                 out = test_fs_duck(newImage, pixels, x, y, high_pixel,
-                                   pixel, False, green)
+                                   pixel, False, high_pixel)
 
         for x in range(width - 1, -1, -1):
             # Get Pixel
@@ -257,45 +338,30 @@ def fill_irregularities(image):
 
             if (high_pixel > pixel):
                 out = test_mp_axe(newImage, pixels, x, y,
-                                  high_pixel, pixel, True, magenta)
+                                  high_pixel, pixel, True, high_pixel)
                 out = test_fs_longduck(newImage, pixels, x, y,
-                                       high_pixel, pixel, True, green)
+                                       high_pixel, pixel, True, high_pixel)
                 out = test_fs_duck(newImage, pixels, x, y, high_pixel,
-                                   pixel, True, green)
+                                   pixel, True, high_pixel)
 
     # Return new image
     return newImage
 
 
-def nearest(image):
-    # Get size
-    scale = 0.5
-    width, height = image.size
-    dstWidth = int(width // 2)
-    dstHeight = int(height // 2)
-
-    # Create new Image and a Pixel Map
-    new = create_image(dstWidth, dstHeight)
-    pixels = new.load()
-
-    lastcolor = None
-
-    # Transform to grayscale
-    for y in range(dstWidth):
-        for x in range(dstHeight):
-            pixels[x, y] = get_pixel(image, int(
-                math.floor(x / scale)), int(math.floor(y / scale)))
-
-    return new
-
-
 def main(argv):
-    img = open_image("aztec.png")
-    # processed = find_ramps(img)
-    processed = fill_irregularities(img)
-    # processed = nearest(img)
-
-    save_image(processed, "out.png")
+    files = [f for f in listdir("./") if isfile(join("./", f))]
+    for file in files:
+        if file == "images.py" or file == "ramps.psd":
+            continue
+        print(file)
+        img = open_image(file)
+        no_irreg = fill_irregularities(img)
+        edges = find_edges(no_irreg)
+        [render, ramps] = create_ramps(edges, img)
+        render.show()
+        # rampImage = draw_ramps(ramps, img)
+        # save_image(rampImage, "out/out-" + file)
+        break
 
 
 if __name__ == "__main__":
