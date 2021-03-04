4d0b71f82431d18383898a48af3efad098d6e782
Alex Pineda <alexpineda86@gmail.com>
2021-03-03 17:27:24 -0800

    fix hd creep edges

--- packages/titan-reactor-shared/map/MapHD.js
+++ packages/titan-reactor-shared/map/MapHD.js
@@ -10,6 +10,7 @@ import {
   LinearFilter,
   ClampToEdgeWrapping,
   sRGBEncoding,
+  DoubleSide,
 } from "three";
 
 import { DDSLoader } from "titan-reactor-shared/image/DDSLoader";
@@ -158,7 +159,54 @@ export default class MapHD {
   }
 
   static renderCreepEdgesTexture(renderer, creepGrp) {
-    const size = Math.ceil(Math.sqrt(creepGrp.length));
+    //0-3 bottomEdge
+    //4 rightEdge
+    //6,11,17,21 top edge
+    //15 left edge
+
+    const bottomEdges = [0, 1, 2, 3];
+    const rightEdges = [4];
+    const topEdges = [6, 11, 17, 21];
+    const leftEdges = [15];
+
+    const getOffset = (grp, tileId) => {
+      const x = 0.5;
+      const y = 0.5;
+
+      if (topEdges.includes(tileId)) {
+        return {
+          x,
+          y: grp.h / 256,
+        };
+      }
+
+      if (bottomEdges.includes(tileId)) {
+        return {
+          x,
+          y: 1 - grp.h / 256,
+        };
+      }
+
+      if (leftEdges.includes(tileId)) {
+        return {
+          y,
+          x: 1 - grp.w / 256,
+        };
+      }
+
+      if (rightEdges.includes(tileId)) {
+        return {
+          y,
+          x: grp.w / 256,
+        };
+      }
+
+      return {
+        x,
+        y,
+      };
+    };
+
     const width = 38;
     const height = 1;
     const ortho = new OrthographicCamera(
@@ -191,14 +239,16 @@ export default class MapHD {
 
       mat.map = texture;
       mat.needsUpdate = true;
+      mat.side = DoubleSide;
       mesh.scale.set(grp.w / 128, grp.h / 128, 1);
-      mesh.position.x = x - width / 2 + grp.w / 256;
+      mesh.position.x = x - width / 2 + getOffset(grp, i).x;
       // if (x >= 1 && x <= 4) {
       //   mesh.position.z = y - height / 2 + 1;
       // } else {
-      mesh.position.z = y - height / 2 + grp.h / 256;
+      mesh.position.z = y - height / 2 + getOffset(grp, i).y;
       // }
       mesh.rotation.z = Math.PI;
+      mesh.rotation.y = Math.PI;
       scene.add(mesh);
       renderer.render(scene, ortho);
       scene.remove(mesh);
